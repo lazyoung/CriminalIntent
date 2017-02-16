53a6c7046af557105304473170f8700583d8f676
lazyoung <lazyoung@gmail.com>
2017-02-17 07:47:27 +0800

    19.4.3

--- app/src/main/java/com/lazyoung/criminalintent/CrimeCameraFragment.java
+++ app/src/main/java/com/lazyoung/criminalintent/CrimeCameraFragment.java
@@ -1,14 +1,18 @@
 package com.lazyoung.criminalintent;
 
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import java.io.IOException;
+import java.util.List;
 
 
 public class CrimeCameraFragment extends Fragment {
@@ -19,6 +23,7 @@ public class CrimeCameraFragment extends Fragment {
 
     @Nullable
     @Override
+    @SuppressWarnings("deprecation")
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime_camera, container, false);
 
@@ -30,7 +35,69 @@ public class CrimeCameraFragment extends Fragment {
         });
 
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceView);
+        SurfaceHolder holder = mSurfaceView.getHolder();
+        // setType() and SURFACE_TYPE_PUSH_BUFFERS are both deprecated, but are required for Camera preview to work on pre-3.0 devices
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                //Tell Camera to use this surface as its preview area
+                try {
+                    if(mCamera != null) {
+                        mCamera.setPreviewDisplay(holder);
+                    }
+                } catch (IOException exception) {
+                    Log.e(TAG, "Error setting up preview display", exception);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+                if(mCamera == null) return;
+
+                //The surface has changed size; update the camera preview size
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size s = getBestSupportedSize(parameters.getSupportedPictureSizes(), width, height);
+                parameters.setPreviewSize(s.width, s.height);
+                mCamera.setParameters(parameters);
+
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                //We can no longer display on this surface, so stop the preview
+                if(mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
+        });
 
         return v;
+    }
+
+    /** A simple algorithm to get the largest size available.For a more *
+     *  robust version, see CameraPreview. java in the ApiDemos
+     *  * sample app from Android. */
+    @SuppressWarnings("deprecation")
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes, int width, int height) {
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Camera.Size s : sizes) {
+            int area = s.width * s.height;
+            if (area > largestArea) {
+                bestSize = s;
+                largestArea = area;
+            }
+        }
+
+        return bestSize;
     }
 }
